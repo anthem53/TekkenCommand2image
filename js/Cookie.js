1c8222f3f27ed1b50b38c26e831525b885ac65a6
anthem53 <anthem53@postech.ac.kr>
2024-05-27 21:47:03 +0900

    feat. add delete button of one history

--- js/Cookie.js
+++ js/Cookie.js
@@ -165,6 +165,14 @@ function setRecentCommandByNum(Num){
     }
 }
 
+function deleteCookieByNum(cookieNum){
+
+    let cookieName = COOKIE_NAME+"_"+cookieNum
+
+    print(cookieName)
+    document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    
+}
 
 
 
--- js/view.js
+++ js/view.js
@@ -141,6 +141,27 @@ function executeDraw(commmandParaResult){
     }
 }
 
+function getEmptyHistoryButton(){
+    let tempBtn = document.createElement("button")
+    tempBtn.type= "button"
+    tempBtn.className = "list-group-item list-group-item-action"
+    tempBtn.innerHTML = "최근 작성한 커맨드 목록이 없습니다."
+    tempBtn.disabled = true;
+
+    return tempBtn
+}
+
+function deleteCookieRow(cookieNum){
+    const targetRowId = "historyRow_"+cookieNum
+    const targetElem = document.getElementById(targetRowId);
+
+    targetElem.remove();
+
+    const commandHistoryList = document.getElementById("commandHistoryList");
+    if (commandHistoryList.innerHTML == ""){
+        commandHistoryList.appendChild(getEmptyHistoryButton())
+    }
+}
 
 /**
  * Cookie를 읽어와서 Cookie History에 표현시켜주는 함수.
@@ -162,11 +183,7 @@ function setRecentCommandHistory(){
     commandHistoryList.innerHTML = "";
 
     if (count == 0){
-        let tempBtn = document.createElement("button")
-        tempBtn.type= "button"
-        tempBtn.className = "list-group-item list-group-item-action"
-        tempBtn.innerHTML = "최근 작성한 커맨드 목록이 없습니다."
-        tempBtn.disabled = true;
+        let tempBtn = getEmptyHistoryButton()
         commandHistoryList.appendChild(tempBtn)
     }
     else{
@@ -174,9 +191,11 @@ function setRecentCommandHistory(){
         for (let i = cookieList.length -1 ; i >= 0  ; i--){
             // [name, number, value] 
             const cookie = cookieList[i]
+            let cookieNum = cookie[1]
             let cookieValue = cookie[2]
 
             let historyRow = document.createElement("div")
+            historyRow.id= "historyRow_"+cookieNum
             historyRow.className= "d-flex flex-row mb-3"
 
             let numberLabel = document.createElement("button")
@@ -192,6 +211,20 @@ function setRecentCommandHistory(){
             tempBtn.innerHTML = cookieValue
             tempBtn.onclick = function(){ setCommandInputFromCookie(cookieValue)}
             historyRow.appendChild(tempBtn)
+
+            let removeButton = document.createElement("button")
+            removeButton.type= "button"
+            removeButton.className = "btn btn-danger ms-2"
+            removeButton.innerHTML = "×"
+            print(cookieNum)
+            removeButton.onclick = function (){
+                deleteCookieRow(cookieNum)
+                deleteCookieByNum(cookieNum)
+            }
+            
+            
+            historyRow.appendChild(removeButton)
+
             commandHistoryList.appendChild(historyRow)
             rowNum += 1
         }
